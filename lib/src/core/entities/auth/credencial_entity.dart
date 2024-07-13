43120ac196c3f61ddc9d3cfd3c14e265d742997f
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2024-07-13 09:49:57 -0300

    criando as entidades de auth

---
+++ lib/src/core/entities/auth/credencial_entity.dart
@@ -0,0 +1,9 @@
+class CredencialEntity {
+  final String email;
+  final String password;
+
+  CredencialEntity({
+    required this.email,
+    required this.password,
+  });
+}
---
+++ lib/src/core/entities/auth/user_entity.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final int id;
+  final String name;
+  final String email;
+  final String? phone;
+
+  const UserEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.phone,
+  });
+
+  @override
+  List<Object?> get props => [id, name, email, phone];
+}
